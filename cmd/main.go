b09dfe66f1f457eb7f3ce8998ea3236947cc3970
Nikita <nikitacharny@yandex.com>
2025-02-15 17:23:51 +0300

    init project

--- cmd/main.go
+++ cmd/main.go
@@ -27,8 +27,6 @@ func main() {
 	// Инициализация экземпляра маршрутизатора
 	router := gin.Default()
 
-	log.Println(configs.AppConfig.YandexDisk.Token)
-
 	// Использование конфигурации CORS в нашем маршрутизаторе
 	router.Use(configs.CorsConfig())
 
